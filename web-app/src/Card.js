5d7500a39d492606e199ae76cdae6442362bf10f
Thibault <thibault.eynard@hotmail.fr>
2019-10-26 13:44:05 +0200

    Clean-up and base Card component

---
+++ web-app/src/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+import './Card.css';
+
+function Card() {
+	return (
+		<img src={logo} className="AppLogo" alt="logo" />
+	);
+}
+
+export default Card;
