8827e540c2aa322e57d915c556e00612451dde96
Thibault <thibault.eynard@hotmail.fr>
2019-11-02 18:40:00 +0100

    On Click Changes

--- web-app/src/components/Card.js
+++ web-app/src/components/Card.js
@@ -2,13 +2,27 @@ import React from 'react';
 import '../style/Card.css';
 
 class Card extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			toggleState: false
+		};
+		
+		this.toggleCard = this.toggleCard.bind(this);
+	}
+
 	render() {
 		return (
-			<div className="CardContainer">
-				<img src={this.props.Picture} class="CardPicture" alt=""/>
+			<div className="CardContainer" onClick={this.toggleCard}>
+				<img src={this.props.Picture} className="CardPicture" alt=""/>
 				<div className="CardFooter"></div>
 			</div>
 		);
+	}
+
+	toggleCard() {
+		this.setState(state => ({toggleState: !state.toggleState}));		
+		console.log(this.state);
 	}
 }
 
